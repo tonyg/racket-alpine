85dde9c8fda28ace9262a2b2f5632270ffa765c3
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-08-02 11:40:09 +0100

    Moderately unpleasant templating of APKBUILD.

--- racket-alpine-packages/build-single-package.sh
+++ racket-alpine-packages/build-single-package.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
+set -xe
+
 pkg="$1"
-cd racket-alpine/$pkg && abuild -r -P $(pwd)/../../../packages
+shift
+
+cd racket-alpine/$pkg
+
+rm -rf APKBUILD common-patches
+cp ../../APKBUILD.template APKBUILD
+cp -a ../../common-patches .
+
+. APKBUILD.vars
+sed -i \
+    -e "s|@@pkgname@@|$pkgname|g" \
+    -e "s|@@pkgdesc@@|$pkgdesc|g" \
+    -e "s|@@depends_dev@@|$depends_dev|g" \
+    -e "s|@@source@@|$source|g" \
+    -e "s|@@sha512sums@@|$sha512sums|g" \
+    APKBUILD
+
+abuild -r -P $(pwd)/../../../packages "$@"
