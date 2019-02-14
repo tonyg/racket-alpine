8f4fd461b496d3c42922170b5ebfc579a22530ff
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2019-02-14 16:30:56 +0000

    Use experimental DOCKER_BUILDKIT "RUN --mount..." feature to shrink images

--- racket-alpine-image/build.sh
+++ racket-alpine-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf packages
 cp -a ../packages .
-docker build -t racket-alpine -f Dockerfile.full .
-docker build -t racket-alpine-minimal -f Dockerfile.minimal .
+DOCKER_BUILDKIT=1 docker build -t racket-alpine -f Dockerfile.full .
+DOCKER_BUILDKIT=1 docker build -t racket-alpine-minimal -f Dockerfile.minimal .
